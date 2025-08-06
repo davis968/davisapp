4a0cb310dd7d3e7eec3ddd9fe6252501726d43c1
davis <davis@Ubuntu200.myguest.virtualbox.org>
2025-08-06 10:07:11 +0300

    Initial commit

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:5000',
+  baseURL: process.env.NODE_ENV === 'production'
+    ? 'https://clinic-1-6rjz.onrender.com'
+    : 'http://127.0.0.1:5000',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
